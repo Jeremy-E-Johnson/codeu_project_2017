5b0f1d5c26ca767dbcc8da7d8eeaf7e528ec5f51
Jeremy-E-Johnson <Jeremy.Johnson14@ncf.edu>
2017-04-16 13:41:55 -0400

    Created new chat/common/RsaAesSha class that implements Encryption. Implements RSA, AES and SHA-256. Currently untested. Encryption was also modified to treat byte[] as main data type for encryption /  decryption.

--- src/codeu/chat/common/Encryption.java
+++ src/codeu/chat/common/Encryption.java
@@ -1,9 +1,20 @@
 package codeu.chat.common;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+
 /**
  * Interface for encrypting and decrypting messages and for hashing strings.
  */
 public interface Encryption {
+
+  /**
+   * Method for setting up RSA encryption.
+   * @return Public key to send to other party.
+   */
+  Key getPublicKey();
 
   /**
    * Uses RSA algorithm to encrypt message. Should be implemented using
@@ -12,39 +23,43 @@ public interface Encryption {
    * @param key Public Key of recipient.
    * @return Message to send to recipient.
    */
-  int publicKeyEncrypt(int message, int key);
+  byte[] publicKeyEncrypt(byte[] message, Key key) throws InvalidKeyException,
+      IllegalBlockSizeException, BadPaddingException;
 
   /**
    * Uses RSA algorithm to decrypt incoming message.
    * @param message Data to be decrypted.
    * @return Message data.
    */
-  int publicKeyDecrypt(int message);
+  byte[] publicKeyDecrypt(byte[] message) throws InvalidKeyException,
+      IllegalBlockSizeException, BadPaddingException;
 
   /**
    * Sets the internal symmetric key for symmetric key encryption.
    * @param key Value to be remembered.
    */
-  void setSymmetricKey(int key);
+  void setSymmetricKey(byte[] key);
 
   /**
    * Encrypts message using stored key.
    * @param message Message to be encrypted.
    * @return Encrypted message.
    */
-  int symmetricKeyEncrypt(int message);
+  byte[] symmetricKeyEncrypt(byte[] message) throws InvalidKeyException,
+      IllegalBlockSizeException, BadPaddingException;
 
   /**
    * Decrypts message using stored key.
    * @param message Message to be decrypted.
    * @return Decrypted message.
    */
-  int symmetricKeyDecrypt(int message);
+  byte[] symmetricKeyDecrypt(byte[] message) throws InvalidKeyException,
+      IllegalBlockSizeException, BadPaddingException;
 
   /**
    * Hash function for hashing passwords and such.
    * @param input Input to the hash.
    * @return Output of the function.
    */
-  int hash(int input);
+  byte[] hash(byte[] input);
 }
---
+++ src/codeu/chat/common/RsaAesSha.java
@@ -0,0 +1,140 @@
+package codeu.chat.common;
+
+import codeu.chat.util.Logger;
+import codeu.chat.common.Encryption;
+import java.security.*;
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+
+
+public class RsaAesSha implements Encryption{
+
+  private final static Logger.Log LOG = Logger.newLog(RsaAesSha.class);
+
+  private Key publicKey;
+  private Key privateKey;
+  private Cipher rsa;
+  private Cipher aes;
+  private SecretKeySpec symmetricKey;
+  private MessageDigest md;
+
+  public RsaAesSha() {
+    try {
+      KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+      kpg.initialize(2048);
+      KeyPair kp = kpg.genKeyPair();
+      publicKey = kp.getPublic();
+      privateKey = kp.getPrivate();
+    } catch(Exception ex) {
+      System.out.println("ERROR: Exception creating key pair. Check log for details.");
+      LOG.error(ex, "Exception creating key pair.");
+    }
+
+    try {
+      rsa = Cipher.getInstance("RSA");
+      aes = Cipher.getInstance("AES");
+      md = MessageDigest.getInstance("SHA-256");
+    } catch(Exception ex) {
+      System.out.println("ERROR: Exception creating Ciphers. Check log for details.");
+      LOG.error(ex, "Exception creating Ciphers.");
+    }
+  }
+
+  public byte[] publicKeyEncrypt(byte[] message, Key pubKey) throws InvalidKeyException,
+      IllegalBlockSizeException, BadPaddingException {
+    try {
+      rsa.init(Cipher.ENCRYPT_MODE, pubKey);
+    } catch(InvalidKeyException ex) {
+      System.out.println("ERROR: Given invalid public key. Check log for details.");
+      LOG.error(ex, "Exception using public key.");
+      throw ex;
+    }
+
+    byte[] cipherData;
+    try {
+      cipherData = rsa.doFinal(message);
+    } catch(Exception ex){
+      System.out.println("ERROR: Exception during public key encryption. Check log for details.");
+      LOG.error(ex, "Exception during public key encryption.");
+      throw ex;
+    }
+    return cipherData;
+  }
+
+  public byte[] publicKeyDecrypt(byte[] message) throws InvalidKeyException,
+      IllegalBlockSizeException, BadPaddingException {
+    try {
+      rsa.init(Cipher.DECRYPT_MODE, privateKey);
+    } catch(InvalidKeyException ex) {
+      System.out.println("ERROR: Given invalid private key. Check log for details.");
+      LOG.error(ex, "Exception using private key.");
+      throw ex;
+    }
+
+    byte[] cipherData;
+    try {
+      cipherData = rsa.doFinal(message);
+    } catch(Exception ex){
+      System.out.println("ERROR: Exception during public key decryption. Check log for details.");
+      LOG.error(ex, "Exception during public key decryption.");
+      throw ex;
+    }
+    return cipherData;
+  }
+
+  public Key getPublicKey(){
+    return publicKey;
+  }
+
+  public void setSymmetricKey(byte[] key){
+    symmetricKey = new SecretKeySpec(key, "AES");
+  }
+
+  public byte[] symmetricKeyEncrypt(byte[] message) throws InvalidKeyException,
+      IllegalBlockSizeException, BadPaddingException {
+    try {
+      aes.init(Cipher.ENCRYPT_MODE, symmetricKey);
+    } catch(InvalidKeyException ex) {
+      System.out.println("ERROR: Given invalid symmetric key. Check log for details.");
+      LOG.error(ex, "Exception using symmetric key.");
+      throw ex;
+    }
+
+    byte[] cipherData;
+    try {
+      cipherData = aes.doFinal(message);
+    } catch(Exception ex){
+      System.out.println("ERROR: Exception during symmetric key encryption. Check log for details.");
+      LOG.error(ex, "Exception during symmetric key encryption.");
+      throw ex;
+    }
+    return cipherData;
+  }
+
+  public byte[] symmetricKeyDecrypt(byte[] message) throws InvalidKeyException,
+      IllegalBlockSizeException, BadPaddingException {
+    try {
+      aes.init(Cipher.DECRYPT_MODE, symmetricKey);
+    } catch(InvalidKeyException ex) {
+      System.out.println("ERROR: Given invalid symmetric key. Check log for details.");
+      LOG.error(ex, "Exception using symmetric key.");
+      throw ex;
+    }
+
+    byte[] cipherData;
+    try {
+      cipherData = aes.doFinal(message);
+    } catch(Exception ex){
+      System.out.println("ERROR: Exception during symmetric key decryption. Check log for details.");
+      LOG.error(ex, "Exception during symmetric key decryption.");
+      throw ex;
+    }
+    return cipherData;
+  }
+
+  public byte[] hash(byte[] data){
+    md.update(data);
+    byte[] value = md.digest();
+    return value;
+  }
+}
