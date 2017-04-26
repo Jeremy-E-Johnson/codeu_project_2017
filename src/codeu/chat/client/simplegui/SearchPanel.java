6ff13ecf435d1f37df79d06fda4fb75d1c2aeec4
sliu27 <susan_liu@brown.edu>
2017-04-25 20:17:28 -0400

    skhjfsa

--- src/codeu/chat/client/simplegui/SearchPanel.java
+++ src/codeu/chat/client/simplegui/SearchPanel.java
@@ -133,6 +133,22 @@ public final class SearchPanel extends JPanel {
       }
     );
   }
+  public void Query(ClientContext myContext, String myquery) {  
+    ClientMessage message = myContext.message;
+    ClientConversation convo = myContext.conversation;
+    List<Message> contents = message.getConversationContents(convo.currentSummary);
+    String[]searchWords = myquery.split(\p{Space}|\p{Punct});
+    for (Message m : contents) {
+        String[]tokens = m.content.split(\p{Space}|\p{Punct});
+        for (String searchw : searchWords) {
+          if (tokens.contains(searchw)) {
+
+          }
+        }
+      }
+
+      console.log
+ }
 
   // TODO implement the proper search calls and remove stubby test search code.
   // Clears panel, sends search call, displays results, updates current search.
